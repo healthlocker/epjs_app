c75f789b2192e770a32831b8d91b755a3b58fb34
robstallion <robertefrancis18@gmail.com>
2017-07-28 12:14:40 +0100

    removes unused code and creates route for care-team/for #6

--- web/router.ex
+++ web/router.ex
@@ -1,21 +1,13 @@
 defmodule EpjsApp.Router do
   use EpjsApp.Web, :router
 
-  pipeline :browser do
-    plug :accepts, ["html"]
-    plug :fetch_session
-    plug :fetch_flash
-    plug :protect_from_forgery
-    plug :put_secure_browser_headers
-  end
-
   pipeline :api do
     plug :accepts, ["json"]
   end
 
-  scope "/api", EpjsApp do
+  scope "/", EpjsApp do
     pipe_through :api
 
-    get "/", PageController, :index
+    get "/care-team/for", CareTeamController, :for
   end
 end
