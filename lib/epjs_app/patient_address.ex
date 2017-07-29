d3919c53fd169f73b4f1ea77638af48e489eed04
robstallion <robertefrancis18@gmail.com>
2017-07-29 11:08:11 +0100

    create file to get patient addresses #1

---
+++ lib/epjs_app/patient_address.ex
@@ -0,0 +1,18 @@
+defmodule EpjsApp.PatientAddress do
+  alias EpjsApp.{Repo, EPJSPatientAddressDetails}
+  import Ecto.Query
+
+  def for(user) do
+    query = from e in EPJSTeamMember,
+      where: e."Patient_ID" == ^user.slam_id
+
+    Repo.all(query)
+  end
+
+  def get_address(%{"service_user" => service_user}) do
+    query = from e in EPJSPatientAddressDetails,
+      where: e."Patient_ID" == ^service_user.slam_id
+
+      Repo.one(query)
+  end
+end
