cc40aa0c32627baf71d7aa8363f08aacae9daf5b
robstallion <robertefrancis18@gmail.com>
2017-07-29 11:01:58 +0100

    remove all hl code and update find epjs user to work with poison call #1 #6

--- lib/epjs_app/carer_connection.ex
+++ lib/epjs_app/carer_connection.ex
@@ -1,41 +1,9 @@
 defmodule EpjsApp.CarerConnection do
-  use Ecto.Schema
-  import Ecto.Changeset
   import Ecto.Query
-  alias EpjsApp.{EPJSUser, ReadOnlyRepo}
+  alias EpjsApp.{EPJSUser, Repo}
 
-  embedded_schema do
-    field :forename
-    field :surname
-    field :date_of_birth
-    field :nhs_number # should this be a string or int? Any other particulars?
-    field :epjs_patient_id, :integer
-  end
-
-  def changeset(changeset, params \\ %{}) do
-    changeset
-    |> cast(params, [:forename, :surname, :date_of_birth, :nhs_number])
-    |> cast_datetime(:date_of_birth)
-    |> validate_required([:forename, :surname, :date_of_birth, :nhs_number])
-    |> validate_slam()
-  end
-
-  defp validate_slam(changeset, options \\ []) do
-    epjs_user = find_epjs_user(changeset)
-
-    changeset = validate_change(changeset, :nhs_number, fn _, _ ->
-      if epjs_user, do: [], else: [{:nhs_number, options[:message] || "details do not match"}]
-    end)
-
-    if epjs_user, do: put_change(changeset, :epjs_patient_id, epjs_user."Patient_ID"), else: changeset
-  end
-
-  defp find_epjs_user(%{changes: changes}) when changes == %{} do
-    nil
-  end
-
-  defp find_epjs_user(%{changes: changes}) do
-    %{forename: forename, surname: surname, date_of_birth: date_of_birth, nhs_number: nhs_number} = changes
+  def find_epjs_user(%{"changes" => changes}) do
+    %{"forename" => forename, "surname" => surname, "date_of_birth" => date_of_birth, "nhs_number" => nhs_number} = changes
 
     query = from e in EPJSUser,
       where: e."Forename" == ^forename
@@ -43,15 +11,14 @@ defmodule EpjsApp.CarerConnection do
         and e."DOB" == ^date_of_birth
         and e."NHS_Number" == ^nhs_number
 
-    ReadOnlyRepo.one(query)
+    Repo.one(query)
   end
 
-  defp cast_datetime(changeset, field) do
-    if value = get_change(changeset, field) do
-      datetime = value |> Timex.parse!("%d/%m/%Y", :strftime) |> DateTime.from_naive!("Etc/UTC")
-      put_change(changeset, field, datetime)
-    else
-      changeset
-    end
+  def find_epjs_user(%{"service_user" => service_user}) do
+    query = from e in EPJSUser,
+      where: e."Patient_ID" == ^service_user.slam_id)
+
+    Repo.one(query)
   end
+
 end
