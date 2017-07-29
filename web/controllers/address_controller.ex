948f0b4ec37aaa8f366ba9aa8228f3162462c22c
robstallion <robertefrancis18@gmail.com>
2017-07-29 11:17:47 +0100

    create controller to deal with calls to epjs address table #1 #6

---
+++ web/controllers/address_controller.ex
@@ -0,0 +1,22 @@
+defmodule EpjsApp.CareTeamController do
+  use EpjsApp.Web, :controller
+  alias EpjsApp.{PatientAddress}
+
+  def for(conn, %{"service_user" => service_user}) do
+  # pattern matches against params for the service user,
+  # grabs the data and then uses it to make a call to EJPS DB
+    params = service_user |> Poison.decode!
+    %{"slam_id" => slam_id} = params
+    care_team = CareTeam.for(%{slam_id: slam_id})
+    json conn, care_team
+  end
+
+  def get_address(conn, %{"service_user" => service_user}) do
+    address =
+      service_user
+      |> Poison.decode!
+      |> PatientAddress.get_address
+
+    json conn, address 
+  end
+end
