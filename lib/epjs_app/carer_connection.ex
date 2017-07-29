49391d9b0870b9f5d848f0fbc5548690df2e3525
katbow <katherine.bowler@gmail.com>
2017-07-29 16:00:08 +0100

    Fix test for carer_connection #1 #6

--- lib/epjs_app/carer_connection.ex
+++ lib/epjs_app/carer_connection.ex
@@ -2,21 +2,20 @@ defmodule EpjsApp.CarerConnection do
   import Ecto.Query
   alias EpjsApp.{EPJSUser, Repo}
 
-  def find_user(%{"changes" => changes}) do
-    %{"forename" => forename, "surname" => surname, "date_of_birth" => date_of_birth, "nhs_number" => nhs_number} = changes
+  def find_user(%{"service_user" => service_user}) do
+    query = from e in EPJSUser,
+    where: e."Patient_ID" == ^service_user.slam_id
+    
+    Repo.one(query)
+  end
 
+  def find_user(user_data) do
+    %{"forename" => forename, "surname" => surname, "date_of_birth" => date_of_birth, "nhs_number" => nhs_number} = user_data
     query = from e in EPJSUser,
       where: e."Forename" == ^forename
         and e."Surname" == ^surname
         and e."DOB" == ^date_of_birth
         and e."NHS_Number" == ^nhs_number
-
-    Repo.one(query)
-  end
-
-  def find_user(%{"service_user" => service_user}) do
-    query = from e in EPJSUser,
-      where: e."Patient_ID" == ^service_user.slam_id
 
     Repo.one(query)
   end
