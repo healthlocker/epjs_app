d2183e5680d02f76a48ed0c93c17636949307d94
robstallion <robertefrancis18@gmail.com>
2017-07-29 12:14:27 +0100

    add correct controller in route

--- web/router.ex
+++ web/router.ex
@@ -12,7 +12,7 @@ defmodule EpjsApp.Router do
     get "/epjsuser/carer-connection/find-epjs-user", EpjsUserController, :find_epjs_user
     get "/epjsteammember/clinician-connection/find-clinician", EpjsTeamMemberController, :find_clinician
     get "/epjsteammember/clinician-connection/get-patients", EpjsTeamMemberController, :get_patients
-    get "/epjspatientaddressdetails/address/get-address", EpjsPatientAddressDetails, :get_address
+    get "/epjspatientaddressdetails/address/get-address", AddressController, :get_address
 
   end
 end
