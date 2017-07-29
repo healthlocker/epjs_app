53bfc5a3993c26e16a7877adea43d6cdd7f27ff8
robstallion <robertefrancis18@gmail.com>
2017-07-29 11:15:56 +0100

    create controller to deal with queries to epjs team member table #1 #6

---
+++ web/controllers/epjs_team_member_controller.ex
@@ -0,0 +1,20 @@
+defmodule EpjsApp.EpjsTeamMemberController do
+  use EpjsApp.Web, :controller
+  alias EpjsApp.{ClinicianConnection}
+
+  def find_clinician(conn, %{"user_data" => user_data}) do
+    decrypted_user_data = user_data
+    |> Poison.decode
+    |> ClinicianConnection.find_clinician
+
+    json conn, decrypted_user_data
+  end
+
+  def get_patients(conn, %{"clinician" => clinician}) do
+    patient_details = clinician
+    |> Poison.decode
+    |> ClinicianConnection.get_patients
+
+    json conn, patient_details
+  end
+end
