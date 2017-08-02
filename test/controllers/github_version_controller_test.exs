bf818364830cf1b53e4fc1d52d62603cf1ad78c5
nelsonic <contact.nelsonic+github@gmail.com>
2017-08-02 11:44:03 +0100

    adds test/controllers/github_version_controller_test.exs for #9

---
+++ test/controllers/github_version_controller_test.exs
@@ -0,0 +1,11 @@
+defmodule EpjsApp.GithubVersionControllerTest do
+  use EpjsApp.ConnCase
+
+  test "GET /_version", %{conn: conn} do
+    {rev, _} = System.cmd("git", ["rev-parse", "HEAD"])
+    version = String.replace(rev, "\n", "")
+    IO.puts version
+    conn = get conn, "/_version"
+    assert text_response(conn, 200) =~ version
+  end
+end
