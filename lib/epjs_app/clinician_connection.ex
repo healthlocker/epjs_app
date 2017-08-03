b9d58b5ab01c4a0f73ccffe8f4317aeef349451e
robstallion <robertefrancis18@gmail.com>
2017-08-03 14:56:23 +0100

    call sanitize_map function to format data correctly #1

--- lib/epjs_app/clinician_connection.ex
+++ lib/epjs_app/clinician_connection.ex
@@ -1,22 +1,22 @@
 defmodule EpjsApp.ClinicianConnection do
   import Ecto.Query
   alias EpjsApp.{EPJSUser, EPJSTeamMember, Repo, DecryptUser}
+  import EpjsApp.Encoder, only: [sanitize_map: 1]
 
   def find_clinician(user_data) do
     [decrypted_user_guid, decrypted_time_str] = DecryptUser.decrypt_user_data(user_data)
     query = from etm in EPJSTeamMember, where: etm."User_Guid" == ^decrypted_user_guid
+    clinician = sanitize_map(Repo.all(query))
 
-    %{decrypted_time_str: decrypted_time_str, decrypted_user_guid: decrypted_user_guid, clinician: Repo.all(query)}
+    %{decrypted_time_str: decrypted_time_str, decrypted_user_guid: decrypted_user_guid, clinician: clinician}
   end
 
   def get_patients(%{"email" => email}) do
-    IO.inspect(email, label: "-----> email")
     ids_query = from e in EPJSTeamMember,
       where: e."Email" == ^email,
       select: e."Patient_ID"
     patient_ids =
       Repo.all(ids_query)
-    IO.inspect(patient_ids, label: "-----> patient_ids")
 
     patients = patient_ids
     |> Enum.map(fn id ->
@@ -24,6 +24,7 @@ defmodule EpjsApp.ClinicianConnection do
       where: e."Patient_ID" == ^id)
     end)
     |> List.flatten
+    |> sanitize_map
 
     %{patient_ids: patient_ids, patients: patients}
   end
