5b6bd64d4169ec9200b3dda9247b958cc9b19a30
robstallion <robertefrancis18@gmail.com>
2017-08-03 14:56:52 +0100

    define sanitize_map function for formatting map/structs correctly #1

--- lib/epjs_app/encoder.ex
+++ lib/epjs_app/encoder.ex
@@ -3,9 +3,17 @@ defmodule EpjsApp.Encoder do
     Enum.map(list, &sanitize_map/1)
   end
 
+  def sanitize_map(nil) do
+    nil
+  end
+
   def sanitize_map(map) do
-    map
-    |> Map.from_struct
-    |> Map.drop([:__meta__, :__struct__])
+    if Map.has_key?(map, :__struct__) do
+      map
+      |> Map.from_struct
+      |> Map.drop([:__meta__, :__struct__])
+    else
+      map
+    end
   end
 end
